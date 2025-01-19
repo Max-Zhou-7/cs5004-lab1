c301d00791286b60cfa19cb8832df97242e03863
Zilu Zhou <ziluzhou7@gmail.com>
2025-01-19 17:02:34 -0500

    second commit

--- src/stock/Stock.java
+++ src/stock/Stock.java
@@ -1,98 +1,106 @@
 package stock;
 
+
+import java.text.DecimalFormat;
+
+/**
+ *
+ */
 public class Stock {
-    private String symbol;
-    private String name;
-    private double costBasis;
-    private double currentPrice;
+  private String symbol;
+  private String name;
+  private double costBasis;
+  private double currentPrice;
 
-    /**
-     * Default constructor
-     */
+/**
+ * Default constructor
+ */
 
-    public Stock(){
-        this.symbol = "abc";
-        this.name = "aaa";
-        this.costBasis = 1.0;
-    }
+public Stock(){
+    this.symbol = "abc";
+    this.name = "aaa";
+    this.costBasis = 1.0;
+}
 
-    /**
-     *
-     * @param symbol: symbol
-     * @param name : company name
-     * @param costBasis : cost basis
-     */
+/**
+ *
+ * @param symbol: symbol
+ * @param name : company name
+ * @param costBasis : cost basis
+ */
 
-    public Stock(String symbol, String name, double costBasis){
-        this.symbol = symbol;
-        this.name = name;
-        this.costBasis = costBasis;
-    }
+public Stock(String symbol, String name, double costBasis){
+    this.symbol = symbol;
+    this.name = name;
+    this.costBasis = costBasis;
+}
 
 
-    /**
-     * Symbol Getter
-     * @return symbol
-     */
-    public String getSymbol(){
-        return this.symbol;
-    }
+/**
+ * Symbol Getter
+ * @return symbol
+ */
+public String getSymbol(){
+    return this.symbol;
+}
 
-    /**
-     * Name Getter
-     * @return name
-     */
-    public String getName(){
-        return this.name;
-    }
+/**
+ * Name Getter
+ * @return name
+ */
+public String getName(){
+    return this.name;
+}
 
-    /**
-     * cost basis Getter
-     * @return costBasis
-     */
-    public double getCostBasis(){
-        return this.costBasis;
-    }
+/**
+ * cost basis Getter
+ * @return costBasis
+ */
+public double getCostBasis(){
+    return this.costBasis;
+}
 
-    /**
-     * Current Price Getter
-     * @return currentPrice
-     */
-    public double getCurrentPrice() {
-        return currentPrice;
-    }
+/**
+ * Current Price Getter
+ * @return currentPrice
+ */
+public double getCurrentPrice() {
+    return currentPrice;
+}
 
-    /**
-     * cost Basis Setter
-     * @param newBasis as new cost basis
-     */
-    public void setCostBasis(double newBasis){
-        this.costBasis = newBasis;
-    }
+/**
+ * cost Basis Setter
+ * @param newBasis as new cost basis
+ */
+public void setCostBasis(double newBasis){
+    this.costBasis = newBasis;
+}
 
-    /**
-     * currentPrice setter
-     * @param newCurrentPrice as new
-     */
-    public void setCurrentPrice(double newCurrentPrice){
-        currentPrice = newCurrentPrice;
-    }
+/**
+ * currentPrice setter.
+ * @param newCurrentPrice as new.
+ */
+public void setCurrentPrice(double newCurrentPrice){
+    currentPrice = newCurrentPrice;
+}
 
-    /**
-     * Change Percent Getter
-     * @return fractional change
-     */
-    public double getChangePercent(){
-        return (getCurrentPrice() - getCostBasis())/getCostBasis();
-    }
+/**
+ * Change Percent Getter
+ * @return fractional change
+ */
+public double getChangePercent(){
+    return (getCurrentPrice() - getCostBasis())/getCostBasis();
+}
 
-    /**
-     * Override string
-     * @return newString
-     */
-    public String toString(){
-        return "name Current Price: \n" + this.name
-                + " Gain/Loss: " + getChangePercent() * 100;
-    }
+/**
+ * Override string
+ * @return newString
+ */
+public String toString(){
+    DecimalFormat moneyformat = new DecimalFormat("$ 0.00");
+    DecimalFormat percentformat = new DecimalFormat("0.00 %");
+    return this.name + " Current Price: " + moneyformat.format(getCurrentPrice()) + "\n"
+            + " Gain/Loss: " + percentformat.format(getChangePercent());
+}
 
 }
